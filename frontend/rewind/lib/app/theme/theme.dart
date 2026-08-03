20359e4753f1fb25efe4bd48cc24843c8a3dcd80
G1-raj <gullurajput18@gmail.com>
2026-08-03 20:02:01 +0530

    Added the theme colors

---
+++ frontend/rewind/lib/app/theme/theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+
+  static const Color theme = Color.fromRGBO(97, 50, 239, 1);
+  static const Color white = Color.fromRGBO(255, 255, 255, 1);
+  static const Color heading = Color.fromRGBO(24, 26, 55, 1);
+  static const Color subHeading = Color.fromRGBO(154, 156, 173, 1);
+
+  static const Color gradientOne = Color.fromRGBO(115, 68, 234, 1);
+  static const Color gradientTwo = Color.fromRGBO(220, 90, 151, 1);
+}
