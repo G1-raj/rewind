0b3fe6f264f49be225b7d7db6c43a27e5846f289
G1-raj <gullurajput18@gmail.com>
2026-08-04 20:12:11 +0530

    Changes in app.dart

--- frontend/rewind/lib/app/app.dart
+++ frontend/rewind/lib/app/app.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:rewind/features/auth/presentation/signin_screen/signin_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(),
+      // home: AuthNavigator(),
+      home: SigninScreen(),
+    );
   }
 }
