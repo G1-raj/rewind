f079961978fd8b93976236cf18011d910a521b21
G1-raj <gullurajput18@gmail.com>
2026-08-05 16:45:36 +0530

    Implemented router

--- frontend/rewind/lib/app/app.dart
+++ frontend/rewind/lib/app/app.dart
@@ -1,16 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:rewind/features/auth/presentation/signin_screen/signin_screen.dart';
+import 'package:rewind/app/router.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.light(),
-      // home: AuthNavigator(),
-      home: SigninScreen(),
+      routerConfig: router,
     );
   }
 }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData.light(),
+//       // home: AuthNavigator(),
+//       // home: SigninScreen(),
+//       home: SignupScreen(),
+//     );
+//   }
+// }
---
+++ frontend/rewind/lib/app/router.dart
@@ -0,0 +1,24 @@
+import 'package:go_router/go_router.dart';
+import 'package:rewind/features/auth/presentation/auth_navigator/auth_navigator.dart';
+import 'package:rewind/features/auth/presentation/signin_screen/signin_screen.dart';
+
+final GoRouter router = GoRouter(
+  initialLocation: '/auth_navigator',
+
+  routes: [
+    GoRoute(
+      path: '/auth_navigator',
+      builder: (context, state) => AuthNavigator()
+    ),
+
+    GoRoute(
+      path: '/signin',
+      builder: (context, state) => SigninScreen(),
+    ),
+
+    GoRoute(
+      path: '/signup',
+      builder: (context, state) => SigninScreen(),
+    )
+  ]
+);
