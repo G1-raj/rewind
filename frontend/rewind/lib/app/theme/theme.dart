741b3e1baf1645afb98f5dc7f7f7992a4823b72e
G1-raj <gullurajput18@gmail.com>
2026-08-08 20:33:29 +0530

    Added new accent color on theme

--- frontend/rewind/lib/app/theme/theme.dart
+++ frontend/rewind/lib/app/theme/theme.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class AppTheme {
 
   static const Color theme = Color.fromRGBO(97, 50, 239, 1);
+  static const Color themeAccent = Color.fromRGBO(241, 234, 253, 1);
   static const Color white = Color.fromRGBO(255, 255, 255, 1);
   static const Color heading = Color.fromRGBO(24, 26, 55, 1);
   static const Color subHeading = Color.fromRGBO(154, 156, 173, 1);
